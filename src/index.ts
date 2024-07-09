36b2d8c7cbdffb9ad5ce48d1c3fd82cdccaf842c
Marian <pidchashymaryan@gmail.com>
2024-07-09 20:44:31 +0300

    created file env.ts with vars like env vars

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,15 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import { PORT } from "./constants";
+import env from "./env";
+
+dotenv.config();
 
 const app = express();
+const PORT = env.PORT || 3000;
 
 app.use(cors({ credentials: true }));
 app.use(bodyParser.json());
@@ -16,6 +21,12 @@ app.use(compression());
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
+server.listen(PORT || 3000, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(env.DATABASE_URL);
+mongoose.connection.on("error", (err: Error) => {
+  console.log(err);
 });
